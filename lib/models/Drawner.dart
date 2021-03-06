fb3c351bf7c63172cf11971e1d76e1cf519bf184
daschow <daschowgames242@hotmail.com>
2021-03-06 02:01:24 +0100

    feat drawer

---
+++ lib/models/Drawner.dart
@@ -0,0 +1,111 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_api_calls/Screen/DoctorListPatientScreen.dart';
+
+void main() => runApp(DrawerApp());
+
+class DrawerApp extends StatelessWidget {
+  final appTitle = 'Inbox';
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: appTitle,
+      home: MyHomePage(title: appTitle),
+      theme: ThemeData(
+        primaryColor: Color(0xFF6200EE),
+      ),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  final String title;
+
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _selectedDestination = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    final theme = Theme.of(context);
+    final textTheme = theme.textTheme;
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      drawer: Drawer(
+        child: ListView(
+          // Important: Remove any padding from the ListView.
+          padding: EdgeInsets.zero,
+          children: <Widget>[
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Text(
+                'Header',
+                style: textTheme.headline6,
+              ),
+            ),
+            Divider(
+              height: 1,
+              thickness: 1,
+            ),
+            ListTile(
+              leading: Icon(Icons.favorite),
+              title: Text('Item 1'),
+              selected: _selectedDestination == 0,
+              onTap: () => selectDestination(0),
+            ),
+            ListTile(
+              leading: Icon(Icons.delete),
+              title: Text('Item 2'),
+              selected: _selectedDestination == 1,
+              onTap: () => selectDestination(1),
+            ),
+            ListTile(
+              leading: Icon(Icons.label),
+              title: Text('Item 3'),
+              selected: _selectedDestination == 2,
+              onTap: () => selectDestination(2),
+            ),
+            Divider(
+              height: 1,
+              thickness: 1,
+            ),
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Text(
+                'Label',
+              ),
+            ),
+            ListTile(
+              leading: Icon(Icons.bookmark),
+              title: Text('Item A'),
+              selected: _selectedDestination == 3,
+              onTap: () => selectDestination(3),
+            ),
+          ],
+        ),
+      ),
+      body: Column(
+        children: [
+          Container(
+            height: 500,
+            child: PatientListScreen(),
+          ),
+        ],
+      ),
+    );
+  }
+
+  void selectDestination(int index) {
+    setState(() {
+      _selectedDestination = index;
+    });
+  }
+}
