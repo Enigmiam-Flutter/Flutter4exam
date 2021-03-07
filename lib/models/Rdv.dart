2f78775062db183e17ca36f3cc9d34f059dab0bd
daschow <daschowgames242@hotmail.com>
2021-03-07 13:55:56 +0100

    feat/adding description field to rdv

--- lib/models/Rdv.dart
+++ lib/models/Rdv.dart
@@ -4,14 +4,17 @@ class Rdv {
   final String IdD;
   final String IdP;
   final String rdv;
+  final String desc;
   final DocumentReference reference;
 
   Rdv.fromMap(Map<String, dynamic> map, {this.reference})
       : assert(map['IdD'] != null),
         assert(map['IdP'] != null),
         assert(map['rdv'] != null),
+        assert(map['Description'] != null),
         IdD = map['IdD'],
         IdP = map['IdP'],
+        desc = map['Description'],
         rdv = map['rdv'];
 
   Rdv.fromSnapshot(DocumentSnapshot snapshot)
